04c8f612283e2561a0599ac307af0b659cc7df25
t-nonomura <nono.develop@gmail.com>
2019-12-03 08:41:30 +0900

    セルのModel追加

---
+++ app/src/main/java/net/storehouse/nono/custom2048/model/Game2048Model.kt
@@ -0,0 +1,39 @@
+package net.storehouse.nono.custom2048.model
+
+data class Game2048Model(
+    val position: Position,
+    val cellValue: CellValue,
+    val direction: Direction
+)
+
+enum class Position {
+    P_1, P_2, P_3, P_4, P_5,
+    P_6, P_7, P_8, P_9, P_10,
+    P_11, P_12, P_13, P_14, P_15,
+    P_16, P_17, P_18, P_19, P_20,
+    P_21, P_22, P_23, P_24, P_25
+}
+
+enum class CellValue {
+    NONE,
+    NUM_1,
+    NUM_2,
+    NUM_4,
+    NUM_8,
+    NUM_16,
+    NUM_32,
+    NUM_64,
+    NUM_128,
+    NUM_256,
+    NUM_512,
+    NUM_1024,
+    NUM_2048,
+}
+
+enum class Direction {
+    NONE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
---
+++ app/src/main/java/net/storehouse/nono/custom2048/view/GamePlayActivity.kt
@@ -0,0 +1,11 @@
+package net.storehouse.nono.custom2048.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GamePlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
